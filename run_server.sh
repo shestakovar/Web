8f436319689442fc8149c76545953be1e0f35f64
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2020-12-01 05:45:31 +0400

    Add balancing

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,10 @@
 . env/bin/activate
 python dish/manage.py collectstatic
 cd dish
-gunicorn --bind 0.0.0.0:8000 dish.wsgi
+gunicorn --bind 0.0.0.0:8000 dish.wsgi &
+DJANGO_SETTINGS_MODULE=dish.settings_read_only gunicorn --bind 0.0.0.0:8001 dish.wsgi &
+DJANGO_SETTINGS_MODULE=dish.settings_read_only gunicorn --bind 0.0.0.0:8002 dish.wsgi &
+while true ; do
+	:
+done
+trap "kill $(jobs -p)" EXIT
