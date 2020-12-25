d00443ae541f8915999a73734f9650a4eaa18e96
U-DESKTOP-8JASDRF\edami <ed.amiyan@ya.ru>
2020-12-25 21:54:00 +0300

    Add login page

--- dishes-vue/src/App.vue
+++ dishes-vue/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <login/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    login
   }
 }
 </script>
@@ -22,5 +21,43 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: #FFEDC4;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+.login_title {
+  font-family: Roboto, sans-serif;
+  font-weight:;
+  font-size: 68px;
+  font-style: normal;
+  font-weight: 200;
+  line-height: 110px;
+  letter-spacing: 4px;
+  text-align: center;
+  text-transform: uppercase;
+  color: black;
+}
+input {
+  width: 30%;
+}
+button {
+  width: 30%;
+  border: 0px;
+}
+button.login {
+  border-radius: 5px;
+  background-color: #6D6678;
+  color: #ffffff;
+}
+
+button.registration {
+  border: 2px solid #6D6678;
+  color: #6D6678;
+  border-radius: 5px;
+  background: none;
+}
+
+a.forgot_pass {
+  color: #898989;
 }
 </style>
